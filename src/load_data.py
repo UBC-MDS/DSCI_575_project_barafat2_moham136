f959caae1e6907d2a3b39d6684c348e49b3d3202
Bright Bello <brightbello04@gmail.com>
2026-04-11 14:58:54 -0700

    finished load_data.py script

--- src/load_data.py
+++ src/load_data.py
@@ -7,17 +7,10 @@ import os
 import pyarrow as pa
 import pyarrow.parquet as pq
 from pathlib import Path
+con = duckdb.connect()
 
-# Download the dataset from Hugging Face and save it as a parquet file for faster loading in the future. 
-# The parquet file will be stored in the `data/processed` directory, while the raw dataset will be cached in `data/raw` by Hugging Face's `datasets` library.    
-RAW_CACHE_DIR = "../data/raw"
-DATA_PATH = Path("../data/processed/reviews.parquet")
-
-HF_DATASET = "McAuley-Lab/Amazon-Reviews-2023"
-HF_NAME = "raw_review_All_Beauty"
-
-
-def download_and_save_parquet(dataset_name, config_name):
+# Define download and save function
+def download_and_save_parquet(dataset_name, config_name, DATA_PATH, RAW_CACHE_DIR):
     print("Downloading dataset from Hugging Face...")
 
     hf_dataset = load_dataset(
@@ -36,17 +29,58 @@ def download_and_save_parquet(dataset_name, config_name):
     return DATA_PATH
 
 
-def load_data():
+# Define load data function
+def load_data(dataset_name, config_name, DATA_PATH, RAW_CACHE_DIR):
     if not DATA_PATH.exists():
         print("Parquet file not found. Downloading...")
-        download_and_save_parquet(HF_DATASET, HF_NAME)
+        download_and_save_parquet(dataset_name, config_name, DATA_PATH, RAW_CACHE_DIR)
     else:
         print("Loading from local parquet...")
 
     return DATA_PATH
 
-con = duckdb.connect()
-data_path = load_data()
-df = con.execute(f"SELECT * FROM read_parquet('{data_path}')").df()
 
 
+
+# Download and save reviews data    
+RAW_CACHE_DIR = "../data/raw"
+REVIEW_PATH = Path("../data/processed/reviews.parquet")
+
+REVIEWS_DATASET = "McAuley-Lab/Amazon-Reviews-2023"
+REVIEWS_NAME = "raw_review_All_Beauty"
+
+review_path = load_data(REVIEWS_DATASET, REVIEWS_NAME, REVIEW_PATH, RAW_CACHE_DIR)
+reviews = con.execute(f"SELECT * FROM read_parquet('{review_path}')").df()
+
+
+# Load metadata
+META_PATH = Path("../data/processed/meta.parquet")
+meta_dataset = "McAuley-Lab/Amazon-Reviews-2023"
+meta_name = "raw_meta_All_Beauty"
+
+meta_path = load_data(meta_dataset, meta_name, META_PATH, RAW_CACHE_DIR)
+metadata = con.execute(f"SELECT * FROM read_parquet('{meta_path}')").df()
+
+
+
+# merging reviews and metadata on parent_asin and saving as merged.parquet
+con.execute("""
+COPY (
+    SELECT
+        r.rating,
+        r.title,
+        r.text,
+        r.verified_purchase,
+        m.title AS product_title,
+        m.average_rating,
+        m.price,
+        m.description,
+        m.store,
+        m.details
+    FROM read_parquet('../data/processed/reviews.parquet') r
+    JOIN read_parquet('../data/processed/meta.parquet') m
+    ON r.parent_asin = m.parent_asin
+)
+TO '../data/processed/merged.parquet'
+(FORMAT PARQUET)
+""")
